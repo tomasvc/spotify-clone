52677e7b186e45ac9433aca62f9cc728447af5a9
tomasvc <tomas.v0147@gmail.com>
2021-07-24 15:29:03 +0100

    Initial commit

--- src/Login.js
+++ src/Login.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const AUTH_URL = `https://accounts.spotify.com/authorize?client_id=a344e815ed30434c80ce17492635db24&response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state`
+const AUTH_URL = `https://accounts.spotify.com/authorize?client_id=a344e815ed30434c80ce17492635db24&response_type=code&redirect_uri=https://flamboyant-williams-be2f38.netlify.app&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state`
 
 export default function Login() {
     return (
